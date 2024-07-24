9cc775894755ade2874dffdc486cfbf2bd0f90cd
François Pagé <francois.page@mail.mcgill.ca>
2024-07-24 00:44:49 -0400

    Added the arrows on the internet module

--- polybar.old/all_bars.sh
+++ polybar.old/all_bars.sh
@@ -3,5 +3,5 @@
 $HOME/.config/polybar.old/polykill
 sleep 1
 $HOME/.config/polybar.old/polybar_toggle bar1
-$HOME/.config/polybar.old/polybar_toggle bar2
+$HOME/.config/polybar.old/polybar_toggle bar3
 

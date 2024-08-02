c9720a74183fe0112690710288c446d2ff0f24b9
François Pagé <francois.page@mail.mcgill.ca>
2024-08-01 22:37:24 -0400

    Rofi unfucked

--- rofi/applets/shared/theme.bash
+++ rofi/applets/shared/theme.bash
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f93665675a28f00604db10a52022fe16b85a93d0efa3a9876a1f03a3450f8ca9
-size 80
+## Current Theme
+
+type="$HOME/.config/rofi/applets/type-1"
+style='style-1.rasi'

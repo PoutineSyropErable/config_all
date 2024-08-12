ea8a123b55cbcb43121867ac185a727df1c0374c
François Pagé <francois.page@mail.mcgill.ca>
2024-08-11 20:47:02 -0400

    Btop With GPU in Terminal.exe now in shortcut

--- admin_gpu_btop.bat
+++ admin_gpu_btop.bat
@@ -1,9 +1,42 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFD1GTQqBM2KpOpET6/326uSTsXE0V+0xa7PP26eFH+0S5ETjcIRj33lV+A==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFD1GTQqBM2KpOpET6/326uSTsXEuV/A2aJvI5paaL/BT3UzwfdYK5k8akcVMOB5KcxulbwN6rHZH1g==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 :: Run Windows Terminal with PowerShell as admin and execute btop
 
 :: Set the GUID of the PowerShell profile in Windows Terminal
-set powershell_guid={61c54bbd-c2c6-5271-96e7-009a87ff44bf}
+set powershell_guid="{61c54bbd-c2c6-5271-96e7-009a87ff44bf}"
 
 :: Run Windows Terminal with the specified PowerShell profile in admin mode and execute btop
-wt.exe -p %powershell_guid% --title "btop" powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList 'btop'"
+wt.exe -p %powershell_guid% --title "btop" btop
 

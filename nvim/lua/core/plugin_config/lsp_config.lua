1fe2a19f9c69f254e27479ae0d7211903e172694
François Pagé <francois.page@mail.mcgill.ca>
2024-10-30 17:48:24 -0400

    updated neovim on laptop

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,8 +1,6 @@
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "solargraph", "ts_ls", "pyright", "clangd" }
+	ensure_installed = { "lua_ls", "solargraph", "ts_ls", "pyright", "clangd" },
 })
-
-
 
 require("mason-tool-installer").setup({
 
@@ -15,40 +13,54 @@ require("mason-tool-installer").setup({
 		"pylint",
 		"ruff",
 
-
 		"clangd",
 		"clang-format",
 		-- "clang-tidy",
 	},
 })
 
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 local lsp_defaults = lspconfig.util.default_config
 
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-	'force',
-	lsp_defaults.capabilities,
-	require('cmp_nvim_lsp').default_capabilities()
-)
+lsp_defaults.capabilities =
+	vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+local cmp = require("cmp")
+-- `/` cmdline setup.
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
 
+-- `:` cmdline setup.
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{
+			name = "cmdline",
+			option = {
+				ignore_cmds = { "Man", "!" },
+			},
+		},
+	}),
+})
 
-lspconfig.bashls.setup{
-  cmd = { "bash-language-server", "start" },
-  filetypes = { "sh", "bash" },
-  root_dir = lspconfig.util.root_pattern(".git", vim.fn.getcwd()),
-  settings = {
-    bash = {
-      useLinting = true,
-    }
-  }
-}
+lspconfig.bashls.setup({
+	cmd = { "bash-language-server", "start" },
+	filetypes = { "sh", "bash" },
+	root_dir = lspconfig.util.root_pattern(".git", vim.fn.getcwd()),
+	settings = {
+		bash = {
+			useLinting = true,
+		},
+	},
+})
 
-
-
-
-
-lspconfig.lua_ls.setup {
+lspconfig.lua_ls.setup({
 	settings = {
 		Lua = {
 			diagnostics = {
@@ -56,39 +68,38 @@ lspconfig.lua_ls.setup {
 			},
 			workspace = {
 				library = {
-					[vim.fn.expand "$VIMRUNTIME/lua"] = true,
-					[vim.fn.stdpath "config" .. "/lua"] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
 				},
 			},
 		},
-	}
-}
+	},
+})
 
-
-lspconfig.pyright.setup {
+lspconfig.pyright.setup({
 
 	settings = {
 		python = {
 			analysis = {
-				typeCheckingMode = "basic",  -- Can be "off", "basic", or "strict"  
+				typeCheckingMode = "basic", -- Can be "off", "basic", or "strict"
 				autoSearchPaths = true,
 				useLibraryCodeForTypes = true,
-			}
-		}
-	}
-}
+			},
+		},
+	},
+})
 
 lspconfig.clangd.setup({
 	cmd = {
 		-- clangd command with additional options
 		"clangd",
-		"--background-index",              -- Enable background indexing
-		"--clang-tidy",                    -- Enable clang-tidy diagnostics
-		"--completion-style=bundled",      -- Style for autocompletion
-		"--cross-file-rename",             -- Support for renaming symbols across files
-		"--header-insertion=iwyu",         -- Include "what you use" insertion
+		"--background-index", -- Enable background indexing
+		"--clang-tidy", -- Enable clang-tidy diagnostics
+		"--completion-style=bundled", -- Style for autocompletion
+		"--cross-file-rename", -- Support for renaming symbols across files
+		"--header-insertion=iwyu", -- Include "what you use" insertion
 	},
-	capabilities = lsp_defaults.capabilities,  -- Auto-completion capabilities
+	capabilities = lsp_defaults.capabilities, -- Auto-completion capabilities
 	filetypes = { "c", "cpp", "objc", "objcpp" },
 	root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
 	settings = {
@@ -98,66 +109,54 @@ lspconfig.clangd.setup({
 	},
 })
 
-
-
-
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-vim.api.nvim_set_keymap('n', '$', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>Br', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-
-
-
-
-
-
-local null_ls = require("null-ls")
-
-null_ls.setup({
-	sources = {
-		-- Formatting for C++ using clang-format
-		null_ls.builtins.formatting.clang_format.with({
-			filetypes = { "c", "cpp", "objc", "objcpp" },
-		}),
-
-		null_ls.builtins.diagnostics.shellcheck,
-		null_ls.builtins.formatting.shfmt,
-
-	},
-})
-
-
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+vim.api.nvim_set_keymap("n", "$", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>Br", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 
 require("lspconfig").solargraph.setup({})
 require("lspconfig").ts_ls.setup({})
 require("lspconfig").gopls.setup({})
 require("lspconfig").tailwindcss.setup({})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 	callback = function(ev)
 		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
 		-- Buffer local mappings.
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', '<space>H', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<space>la', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<space>lr', vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set('n', '<space>ll', function()
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "<space>H", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<space>la", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>lr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>ll", function()
 			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 		end, opts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<space>f', function()
-			vim.lsp.buf.format { async = true }
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format({ async = true })
 		end, opts)
+	end,
+})
+
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.hl", "hypr*.conf" },
+	callback = function(event)
+		print(string.format("starting hyprls for %s", vim.inspect(event)))
+		vim.lsp.start({
+			name = "hyprlang",
+			cmd = { "hyprls" },
+			root_dir = vim.fn.getcwd(),
+		})
 	end,
 })

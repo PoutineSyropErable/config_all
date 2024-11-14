9daec2b8fdcf9b79eb286dcb24ef35f89b2d08b7
François Pagé <francois.page@mail.mcgill.ca>
2024-11-14 18:43:53 -0500

    Removed lf from tracking to add as submodules

--- lf/cleaner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# cat /home/francois/.config/lf/empty.txt &
-exec kitty +kitten icat --clear --stdin no --silent --transfer-mode file </dev/null >/dev/tty
--- lf/cleaner_tmux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-tty_device=$(tmux display-message -p '#{pane_tty}' 2>/dev/null || tty)
-exec kitty +kitten icat --clear --stdin no --silent --transfer-mode file </dev/null >"$tty_device"
